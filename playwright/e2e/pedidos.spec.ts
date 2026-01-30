ff872327f25daeb5451d416010af4fb65e637625
Jackson Mendes <jacksontestercode@gmail.com>
2026-01-30 10:37:24 -0300

    fix: corrige typo de APROVADU para APROVADO

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -7,27 +7,19 @@ import { test, expect } from '@playwright/test'
 test('deve consultar um pedido aprovado', async ({ page }) => {
   // Arrange
   await page.goto('http://localhost:5173/')
-  
-  // Checkpoint (faz parte da preparação)
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
-  // Act
+  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
   await page.getByTestId('search-order-id').fill('VLO-MM4DYL')
-
-  // Act
   await page.getByTestId('search-order-button').click()
-  
+
   // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-MM4DYL')
-  
-  // Assert
+
   await expect(page.getByTestId('order-result-status')).toBeVisible()
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
 })
