aba96960fde7d31e1d5f8e5f330db87dce698071
Jackson Mendes <jacksontestercode@gmail.com>
2026-02-06 10:09:29 -0300

    refactor(test-data): transformar order em objeto com dados completos

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -18,22 +18,119 @@ test.describe('Consulta de Pedido', () => {
   })
 
   test('deve consultar um pedido aprovado', async ({ page }) => {
+
     // Test Data
-    const order = 'VLO-MM4DYL'
+    const order = {
+      number: 'VLO-MM4DYL',
+      status: 'APROVADO',
+      color: 'Midnight Black',
+      wheel: 'sport Wheels',
+      customer: {
+        name: 'Jackson Mendes',
+        email: 'jacksontestercode@gmail.com',
+      },
+      payment: 'À Vista'
+    }
 
     // Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
     await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
     // Assert
-    const containerPedido = page.getByRole('paragraph')
-    .filter({ hasText: /^Pedido$/ })
-    .locator('..')
+    // const containerPedido = page.getByRole('paragraph')
+    // .filter({ hasText: /^Pedido$/ })
+    // .locator('..')
 
-    await expect(containerPedido).toBeVisible({ timeout: 10_000 })
-    
+    // await expect(containerPedido).toBeVisible({ timeout: 10_000 })
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheel}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
   })
 
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+
+    // Test Data
+    const order = {
+      number: 'VLO-H4N370',
+      status: 'REPROVADO',
+      color: 'Lunar White',
+      wheel: 'sport Wheels',
+      customer: {
+        name: 'Esteve Jobs',
+        email: 'jobs@aple.com',
+      },
+      payment: 'À Vista'
+
+    }
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    // Assert
+    // const containerPedido = page.getByRole('paragraph')
+    // .filter({ hasText: /^Pedido$/ })
+    // .locator('..')
+
+    // await expect(containerPedido).toBeVisible({ timeout: 10_000 })
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheel}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+  })
 
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
     // Test Data
