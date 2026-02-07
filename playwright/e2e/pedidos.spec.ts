eb8e56a4ed9c0f64f863377a240fad29c6aee7d4
Jackson Mendes <jacksontestercode@gmail.com>
2026-02-07 09:40:49 -0300

    test: adiciona teste para consultar pedido em análise

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -132,6 +132,64 @@ test.describe('Consulta de Pedido', () => {
 
   })
 
+  test('deve consultar um pedido em análise', async ({ page }) => {
+
+    // Test Data
+    const order = {
+      number: 'VLO-TZTIJC',
+      status: 'EM_ANALISE',
+      color: 'Glacier Blue',
+      wheel: 'aero Wheels',
+      customer: {
+        name: 'João da Silva',
+        email: 'joao@velo.df',
+      },
+      payment: 'À Vista'
+
+    }
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    // Assert
+    // const containerPedido = page.getByRole('paragraph')
+    // .filter({ hasText: /^Pedido$/ })
+    // .locator('..')
+
+    // await expect(containerPedido).toBeVisible({ timeout: 10_000 })
+
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheel}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
+
+  })
+
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
     // Test Data
     const order = generateOrderCode()
